3a2a56b910a99dff174844de241fbaf0be85c8a9
Nicholas <superpanda7@yahoo.com>
2018-05-19 10:52:57 -0700

    Removed irrelevant files

--- js/ko.js
+++
@@ -1,9 +0,0 @@
-var listValues = {
-  optionValues : ["The Bronze Fonz", "Shakers Cigar Bar", "International Exports Ltd.", "Pabst Mansion", "Art Smart's"],
-  selectedOptionValue : ko.observable()
-};  
-
-  
-ko.applyBindings(listValues);
-
-
